ae5f87148796f1e7d4d4add9338fb54e36082dfc
阳眼的熊 <237505990@qq.com>
2017-12-08 18:59:21 +0800

    修改
    修改

---
+++ GetVolumeLevelsFromeSampleBuffer.podspec
@@ -0,0 +1,20 @@
+
+Pod::Spec.new do |s|
+s.name        = 'GetVolumeLevelsFromeSampleBuffer'
+s.version     = '1.0.1'
+s.authors     = { "阳眼的熊" => "237505990@qq.com" }
+s.homepage    = 'https://github.com/doubleYang1020/GetVolumeLevelsFromeSampleBuffer'
+s.summary     = 'this is get volumeLevels from sampleBuffer engine'
+s.source      = { :git => 'https://github.com/doubleYang1020/GetVolumeLevelsFromeSampleBuffer',:tag => s.version.to_s }
+s.license     = { :type => "MIT", :file => "LICENSE" }
+
+s.platform = :ios, '8.0'
+s.requires_arc = true
+s.source_files = 'GetVolumeLevelsFromeSampleBuffer'
+s.public_header_files = 'GetVolumeLevelsFromeSampleBuffer/*.h'
+
+s.ios.deployment_target = '8.0'
+s.frameworks = 'AVFoundation'
+end
+
+
--- GetVolumeLevelsFromeSampleBuffer/SampleBufferToVolumeLevelsEngine.m
+++ GetVolumeLevelsFromeSampleBuffer/SampleBufferToVolumeLevelsEngine.m
@@ -7,24 +7,16 @@
 //
 
 #import "SampleBufferToVolumeLevelsEngine.h"
-
 @implementation SampleBufferToVolumeLevelsEngine
-
-
-
 +(float)getVolumeLevelsFromeSampleBuffer:(CMSampleBufferRef)sampleBuffer{
     AudioBufferList bufferList;
     int listSize = sizeof(bufferList);
     CMBlockBufferRef blockBuffer;
-    
     OSStatus result;
     result = CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(sampleBuffer, nil, &bufferList, listSize, nil, nil, kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment, &blockBuffer);
-    
     if (result == 0) {
-        
         for( int y=0; y< bufferList.mNumberBuffers; y++ ){
             AudioBuffer audioBuffer = bufferList.mBuffers[y]; 
-            
             Byte *frame = (Byte *)audioBuffer.mData;
             float sum = 0;
             int d = audioBuffer.mDataByteSize/2;
@@ -34,13 +26,10 @@
                 long x2 = frame[i*2];
                 short int w = x1 | x2;
                 sum += w*w;
-                
-                
             } 
             NSLog(@"audio_volume = %lf",(sqrtf(sum/d)));
             return (sqrtf(sum/d));
         } 
-        
     }
     return 0.0;
 }
