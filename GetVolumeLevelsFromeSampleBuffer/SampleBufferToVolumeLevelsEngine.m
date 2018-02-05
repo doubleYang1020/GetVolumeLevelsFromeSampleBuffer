e5af5d22e7a44fadeac60c50cec0b3cca7cd7f8b
Chen Hao <hao.chen@thel.co>
2018-02-05 11:12:06 +0800

    Fix memory leak

--- GetVolumeLevelsFromeSampleBuffer/SampleBufferToVolumeLevelsEngine.m
+++ GetVolumeLevelsFromeSampleBuffer/SampleBufferToVolumeLevelsEngine.m
@@ -8,47 +8,22 @@
 
 #import "SampleBufferToVolumeLevelsEngine.h"
 @implementation SampleBufferToVolumeLevelsEngine
-//+(float)getVolumeLevelsFromeSampleBuffer:(CMSampleBufferRef)sampleBuffer{
-//    AudioBufferList bufferList;
-//    int listSize = sizeof(bufferList);
-//    CMBlockBufferRef blockBuffer;
-//    OSStatus result;
-//    result = CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(sampleBuffer, nil, &bufferList, listSize, nil, nil, kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment, &blockBuffer);
-//    if (result == 0) {
-//        for( int y=0; y< bufferList.mNumberBuffers; y++ ){
-//            AudioBuffer audioBuffer = bufferList.mBuffers[y]; 
-//
-//            Byte *frame = (Byte *)audioBuffer.mData;
-//            float sum = 0;
-//            int d = audioBuffer.mDataByteSize/2;
-//            for(long i=0; i < d; i++) 
-//
-//            {
-//                long x1 = frame[i*2+1]<<8; 
-//
-//                long x2 = frame[i*2];
-//                short int w = x1 | x2;
-//                sum += w*w;
-//            } 
-//
-//            NSLog(@"audio_volume = %lf",(sqrtf(sum/d)));
-//            return (sqrtf(sum/d));
 
 +(float)getVolumeLevelsFromeSampleBuffer:(CMSampleBufferRef)sampleBuffer{
     CMFormatDescriptionRef formatDescription = CMSampleBufferGetFormatDescription(sampleBuffer);
     const AudioStreamBasicDescription asbd = *CMAudioFormatDescriptionGetStreamBasicDescription(formatDescription);
-    
+
     AudioBufferList bufferList;
     int listSize = sizeof(bufferList);
     CMBlockBufferRef blockBuffer;
     OSStatus result;
     result = CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(sampleBuffer, nil, &bufferList, listSize, nil, nil, kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment, &blockBuffer);
-    
+
     float avgLevel = 0.0;
     if (result == 0) {
         AudioBuffer audioBuffer = bufferList.mBuffers[0];
         if (asbd.mFormatFlags & kAudioFormatFlagIsFloat) { // float
-            
+
             float* x = (float*) audioBuffer.mData;
             float sum = 0;
             int count = audioBuffer.mDataByteSize / sizeof(float);
@@ -56,9 +31,9 @@
                 sum += (*x) * (*x);
                 x++;
             }
-            
+
             avgLevel = sqrtf(sum / count);
-            
+
         }
         else {
             assert(asbd.mFormatFlags & kLinearPCMFormatFlagIsSignedInteger); // signed int
@@ -76,14 +51,17 @@
                     x2 = frame[i * 2] << 8;
                 }
                 short int w = x1 | x2;
-                
+
                 sum += w*w;
             }
-            
+
             avgLevel = sqrtf(sum/d);
         }
-        
+
     }
+
+    CFRelease(blockBuffer);
+
     return avgLevel;
 }
 @end
